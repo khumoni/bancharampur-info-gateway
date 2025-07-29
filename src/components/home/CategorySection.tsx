bac739ea748ad88acfd45e510bd83d1de0037cd2
khumoni <127308269+khumoni@users.noreply.github.com>
2025-07-29 20:11:34 +0600

    Update CategorySection.tsx

--- src/components/home/CategorySection.tsx
+++ src/components/home/CategorySection.tsx
@@ -43,7 +43,6 @@ export const CategorySection: React.FC<CategorySectionProps> = ({
                   </li>
               );
             })}
-              
            </ul>
           </ScrollArea>
         </CardContent>
